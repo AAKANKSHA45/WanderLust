98fea201d364d05e23db4c302261bd3650877e4b
Aakanksha Yadav <aakanksha4533@gmail.com>
2026-05-07 16:43:22 +0530

    review routes file is created

--- app.js
+++ app.js
@@ -8,20 +8,18 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 
 
-const listingSchema = require("./schema.js")
-const {reviewSchema} = require("./schema.js")
-// or 
-// const {listingSchema,reviewSchema} = require("./schema.js")
 
-
-const Listing = require("./models/listing.js")
-const Review = require("./models/review.js")
 
 const path = require("path");
 const ejs = require ("ejs");
-const wrapAsync = require("./utils/wrapAsync.js");
 
+
+// requiring routes file
 const listings = require("./routes/listing.js")
+const reviews = require("./routes/review.js")
+
+
+
 
 
 main().then(()=>{
@@ -30,10 +28,11 @@ main().then(()=>{
     console.log(err);
 })
 
-
 async function main (){
     await mongoose.connect(MONGO_URL);
 }
+
+
 
 app.set("view engine" , "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -68,54 +67,14 @@ app.get("/" ,(req,res)=>{
 
 
 
-const validateReview = (req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body) //joi :- validating upcoming data 
-    if (error){
-        let errMsg = error.details.map((el) =>el.message).join(",") //finding  exact message of error
-    throw new ExpressError(400 , errMsg);
-    }else{
-        next(); //go to the NEXT middleware or route.
-    }
-}
+
 
 // listings routes
 app.use("/listings" ,listings);
 
+// review routes
+app.use("/listings/:id/reviews" , reviews);
 
-
-// REVIEW
-// post route
-// since listing and review have one to many relation se we have to store review(obj id) in listing too!!!
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req , res)=>{
-    let listing = await Listing.findById(req.params.id);
-
-    // creating new review
-    let newReview = new Review (req.body.review);
-
-    // putting review into listing
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    res.redirect(`/listings/${req.params.id}`);//redirecting to show page
-    // or
-    //  res.redirect(`/listings/${listing._id}`);
-
-
-}));
-
-
-// delete review route
-app.delete("/listings/:id/reviews/:reviewId", async(req,res)=>{
-    let {id , reviewId} = req.params;
-
-    // update not delete because we are not deleting the listing we are updating it by deleting its review
-    await Listing.findByIdAndUpdate(id,  {$pull : {reviews : reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-
-
-})
 
 
 
---
+++ routes/review.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require("../models/review.js")
+const Listing = require("../models/listing.js")
+
+
+const {reviewSchema} = require("../schema.js");
+// or 
+// const {listingSchema,reviewSchema} = require("./schema.js")
+
+
+
+// validation fn to validate review
+const validateReview = (req,res,next)=>{
+    let {error} = reviewSchema.validate(req.body) //joi :- validating upcoming data 
+    if (error){
+        let errMsg = error.details.map((el) =>el.message).join(",") //finding  exact message of error
+    throw new ExpressError(400 , errMsg);
+    }else{
+        next(); //go to the NEXT middleware or route.
+    }
+}
+
+
+// post route
+// since listing and review have one to many relation se we have to store review(obj id) in listing too!!!
+router.post("/",validateReview,wrapAsync(async(req , res)=>{
+    let listing = await Listing.findById(req.params.id);
+
+    // creating new review
+    let newReview = new Review (req.body.review);
+
+    // putting review into listing
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+    res.redirect(`/listings/${req.params.id}`);//redirecting to show page
+    // or
+    //  res.redirect(`/listings/${listing._id}`);
+
+
+}));
+
+
+// delete review route
+router.delete("/:reviewId", async(req,res)=>{
+    let {id , reviewId} = req.params;
+
+    // update not delete because we are not deleting the listing we are updating it by deleting its review
+    await Listing.findByIdAndUpdate(id,  {$pull : {reviews : reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+
+
+})
+
+module.exports = router;
